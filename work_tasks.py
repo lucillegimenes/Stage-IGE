4aa425766ea1e6aa53954b57cb96105ae8dd52d8
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2021-11-19 11:00:10 +0100

    Update work_tasks.py

--- work_tasks.py
+++ work_tasks.py
@@ -378,7 +378,7 @@ def calibrate_inversion_from_consensus(gdirs, vol_bias=1, ignore_missing=True,
                          odf1.vol_itmix_m3, odf1.oggm, a_bounds[0],
                          odf2.vol_itmix_m3, odf2.oggm, a_bounds[1]))
         if apply_fs_on_mismatch and fs == 0 and odf2.oggm > odf2.vol_itmix_m3:
-            return calibrate_inversion_from_consensus(gdirs,
+            return calibrate_inversion_from_consensus(gdirs,vol_bias=vol_bias
                                                       ignore_missing=ignore_missing,
                                                       fs=5.7e-20, a_bounds=a_bounds,
                                                       apply_fs_on_mismatch=False,
